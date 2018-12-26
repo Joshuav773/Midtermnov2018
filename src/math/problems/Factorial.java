1db330766f98252c9211e318ad02aea65598f4d2
joshuav773 <joshuav773@gmail.com>
2018-12-25 21:36:40 -0500

    Joshua's midterm

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -1,5 +1,7 @@
 package math.problems;
 
+import java.util.Scanner;
+
 /**
  * Created by mrahman on 04/02/18.
  */
@@ -11,6 +13,19 @@ public class Factorial {
          * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
          *
          */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Provide an Integer number to Find it's FACTORIAL");
+        int n = sc.nextInt(); // provide int value to find factorial
 
+        long result = fact(n);
+
+        System.out.println("the factorial of " + n + " is " + result);
+    }
+
+    public static long fact(int n) {
+        if (n == 0)
+            return 1;
+        else
+            return n * fact(n - 1);
     }
 }
--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -5,7 +5,17 @@ public class Fibonacci {
          /*
           Write 40 Fibonacci numbers with java.
          */
+        int n = 40, t1 = 0, t2 = 1;
+        System.out.print("First " + n + " terms: ");
 
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(t1 + " + ");
+
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
 
     }
 }
