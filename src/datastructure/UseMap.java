2fc33da0bcfee42b09baaed1ccf8b68d3a210d78
joshuav773 <joshuav773@gmail.com>
2018-12-28 15:10:39 -0500

    joshua's midterm

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,16 +1,69 @@
 package datastructure;
 
+import java.util.*;
+
 public class UseMap {
 
 	public static void main(String[] args) {
-		/*
-		 * Demonstrate how to use Map that includes storing and retrieving elements.
-		 * Add List<String> into a Map. Like, Map<String, List<string>> list = new HashMap<String, List<String>>();
-		 * Use For Each loop and while loop with Iterator to retrieve data.
-		 *
-		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
-		 */
+        /*
+         * Demonstrate how to use Map that includes storing and retrieving elements.
+         * Add List<String> into a Map. Like, Map<String, List<string>> list = new HashMap<String, List<String>>();
+         * Use For Each loop and while loop with Iterator to retrieve data.
+         *
+         * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
+         */
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Joshua");
+        map.put(2, "Luis");
+        map.put(3, "Mafi");
+        map.put(4, "Arif");
 
-	}
+        System.out.println("using for each loop to get keys and values");
+
+        for(Map.Entry entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+        System.out.println("Using iterator to get keys and values");
+
+        Iterator it = map.entrySet().iterator();
+        while(it.hasNext()){
+           Object keys = it.next();
+            System.out.println(keys);
+
+        }
+
+        List<String> chevyModels = new ArrayList<>();
+        chevyModels.add("Camaro");
+        chevyModels.add("Corvette");
+        chevyModels.add("Duramax");
+
+        List<String> fordModels = new ArrayList<>();
+        fordModels.add("Scape");
+        fordModels.add("Mustang");
+        fordModels.add("Raptor");
+
+        List<String> teslaModels = new ArrayList<>();
+       teslaModels.add("Model 3");
+       teslaModels.add("Model S");
+       teslaModels.add("Model X");
+
+        HashMap<String, List<String>> makesAndModles = new LinkedHashMap<>();
+        makesAndModles.put("Chevy", chevyModels);
+        makesAndModles.put("Ford", fordModels);
+        makesAndModles.put("Tesla", teslaModels);
+
+        System.out.println("Heres my 3 favorite car makes with their most popular models");
+        System.out.println();
+        for (HashMap.Entry<String, List<String>> entry : makesAndModles.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+
+
+
+
+    }
+
 
 }
--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,5 +1,9 @@
 package datastructure;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
 
 	public static void main(String[] args) {
@@ -8,6 +12,24 @@ public class UseQueue {
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 * 
 		 */
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(25);
+        queue.add(26);
+        queue.add(27);
+        queue.add(28);
+
+        System.out.println("Get elements by using for each loop");
+        for (Integer X : queue){
+            System.out.println(X);
+        }
+        System.out.println("Getting elements by using Iterator ");
+        Iterator it = queue.iterator();
+        while(it.hasNext()){
+            int i = (Integer) it.next();
+            System.out.println(i);
+        }
+
+        System.out.println("I want to peek into queue and the value is = " + queue.peek());
 
 	}
 
