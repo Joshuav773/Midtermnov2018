f44ac542893ad271a874c0dbb7e78384338a1e5c
joshuav773 <joshuav773@gmail.com>
2018-12-27 20:52:38 -0500

    Joshua's midterm

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,4 +1,8 @@
 package datastructure;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 
 public class UseArrayList {
 
@@ -9,6 +13,38 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
+        List<String> students = new ArrayList<>();
+         students.add("Joshua");
+         students.add("Luis");
+         students.add("Mafi");
+         students.add("Arif");
+
+        System.out.println("getting elements using for each loop");
+         for (Object x : students){
+             System.out.println(x);
+         }
+
+        System.out.println("getting elements using Iterator and while loop");
+        Iterator<String> it = students.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+        students.remove(students.get(2));
+
+        System.out.println("getting elements after using remove method using for each loop");
+        for (Object y : students) {
+            System.out.println(y);
+        }
+        System.out.println("getting elements after using remove method using Iterator and while loop");
+        Iterator<String> itr = students.iterator();
+        while (itr.hasNext()){
+            System.out.println(itr.next());
+        }
+
+
+
+
 	
 
 	}
